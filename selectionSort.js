7aa4d4d5b76bce57fcbb1673fe4838aef9c7eb05
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-10-09 09:25:05 +0200

    Add selectionSort alg

---
+++ selectionSort.js
@@ -0,0 +1,15 @@
+function selectionSort(array) {
+  for (let i = 0; i < array.length - 1; i++) {
+
+    let minIndex = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }     
+    }
+    [array[i], array[minIndex]] = [array[minIndex], array[i]];
+  }
+  return array;
+}
+
+console.log(selectionSort([23, 12, 1, 45, 32, 67, 34]))
