db70496e348bf412e763a04ad4c7cb22c49fab5f
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-10-09 11:05:50 +0200

    Update

--- bubbleSort.js
+++ bubbleSort.js
@@ -2,12 +2,14 @@ function bubbleSort(array) {
   let isSorted = false;
   while (!isSorted) {
     isSorted = true;
-    for (let i = 0; i < array.length -1 ; i++) {
+    for (let i = 0; i < array.length - 1; i++) {
       if (array[i] > array[i + 1]) {
-        [array[i], array[i + i]] =[array[i + 1], array[i]];
+        [array[i], array[i + 1]] = [array[i + 1], array[i]];
         isSorted = false;
       }
     }
   }
   return array;
 }
+
+console.log(bubbleSort([23, 45, 12, 1, 6, 4, 8, 65]))
