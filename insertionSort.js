5a320b465fcc3f010db0e9a82bf857c7d80109cd
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-10-09 10:26:22 +0200

    Add insertionSort

---
+++ insertionSort.js
@@ -0,0 +1,12 @@
+function insertionSort(array) {
+  for (let i = 1; i < array.length; i++) {
+    let j = i;
+    while (j > 0 && array[j] < array[j - 1]) {
+      [array[j - 1], array[j]] = [array[j], array[j - 1]];
+      j--;
+    }
+  }
+  return array;
+}
+
+console.log(insertionSort([23, 43, 10, 323, 423, 1, 2, 6]))
