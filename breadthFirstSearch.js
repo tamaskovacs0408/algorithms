b1a9fe72408a1b695e4997154bd804d951e97197
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-10-14 11:08:00 +0200

    Update

--- breadthFirstSearch.js
+++ breadthFirstSearch.js
@@ -1,2 +1,10 @@
 let graph;
 const nodes = 5;
+let visited = new Array(nodes);
+
+const createGraph = (nodes) => {
+  graph = new Array(nodes);
+  for (let i = 0; i < graph.length; i++) {
+    graph[i] = new Array(nodes);
+  }
+}
