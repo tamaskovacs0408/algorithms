5c374db757d5d340fd3fedd44a2c2acdf4362ead
Tamás Kovács <kovacs.tamas0408@gmail.com>
2022-12-23 10:32:37 +0100

    Refactor code, easier version

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,15 +1,17 @@
-function bubbleSort(array) {
-  let isSorted = false;
-  while (!isSorted) {
-    isSorted = true;
-    for (let i = 0; i < array.length - 1; i++) {
-      if (array[i] > array[i + 1]) {
-        [array[i], array[i + 1]] = [array[i + 1], array[i]];
-        isSorted = false;
+let bubbleArr = [23, 45, 12, 1, 6, 4, 8, 65];
+
+function bubbleSort(arr) {
+  let temp;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    for (let j = 0; j <= i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
       }
     }
   }
-  return array;
+  return arr;
 }
 
-console.log(bubbleSort([23, 45, 12, 1, 6, 4, 8, 65]))
+console.log(bubbleSort(bubbleArr));
